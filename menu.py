a00905bebc4a43668b5509e328ebb72db06ea6e5
Grzeslaw115 <117384817+Grzeslaw115@users.noreply.github.com>
2024-03-12 00:09:48 +0100

    Added button to stop music

--- menu.py
+++ menu.py
@@ -12,32 +12,50 @@ clock = pygame.time.Clock()
 # Load media
 pygame.mixer.music.load("audio/menu.mp3")
 pygame.mixer.music.play(-1)
+music_on = True
 
 background = pygame.image.load("graphics/menu/background.png").convert()
-text_title = title_font.render("Miners' Bastion", True, 'purple')
+music = pygame.image.load("graphics/menu/music_on.png").convert_alpha()
+text_title = title_font.render("Miners' Bastion", True, 'yellow')
 
 background_y = 800
-last_change = 0
+moving_up = True
 
 # Main loop
 while True:
+
+    dt = clock.tick(60) / 1000
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.mixer.music.stop()
             pygame.quit()
             exit()
 
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                if 1024 - music.get_width() - 10 <= event.pos[0] <= 1024 - 10 and 10 <= event.pos[1] <= 10 + music.get_height():
+                    if music_on:
+                        music = pygame.image.load("graphics/menu/music_off.png").convert_alpha()
+                        pygame.mixer.music.stop()
+                        music_on = False
+                    else:
+                        music = pygame.image.load("graphics/menu/music_on.png").convert_alpha()
+                        pygame.mixer.music.play(-1)
+                        music_on = True
+
     screen.blit(background, (0, 0))
     screen.blit(text_title, (1024 / 2 - text_title.get_width() / 2, background_y))
+    screen.blit(music, (1024 - music.get_width() - 10, 10))
 
-    if last_change == 0:
-        background_y -= 15
-        last_change = 1
-        pygame.time.wait(500)
+    if moving_up:
+        background_y -= 60 * dt
+        if background_y <= 800:
+            moving_up = False
     else:
-        background_y += 15
-        last_change = 0
-        pygame.time.wait(500)
+        background_y += 60 * dt
+        if background_y >= 850:
+            moving_up = True
 
     pygame.display.update()
     clock.tick(60)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,2 +1,1 @@
-
-# Load media
+800

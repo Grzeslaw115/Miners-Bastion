9d2298c912a01dd3e4b1ad74b3cf58a26a683a09
Krzysztof Gołuchowski <kgoluchowski@student.agh.edu.pl>
2024-03-16 16:39:35 +0100

    fix level selection symetry

--- levelSelection.py
+++ levelSelection.py
@@ -26,27 +26,27 @@ def main():
                 sys.exit()
 
             if event.type == pygame.MOUSEBUTTONDOWN:
-                if 1024 / 3 - level1img.get_width() <= event.pos[0] <= 1024 / 3 and 1024 / 2 - level1img.get_height() / 2 <= event.pos[1] <= 1024 / 2 + level1img.get_height() / 2:
+                if 64 <= event.pos[0] <= 64 + level1img.get_width() and 1024 / 2 - level1img.get_height() / 2 <= event.pos[1] <= 1024 / 2 + level1img.get_height() / 2:
                     button_click_sound.play()
                     level1.main()
                     pygame.quit()
                     exit()
 
-                elif 1024 / 3 + 64 <= event.pos[0] <= 1024 / 3 + 64 + level2img.get_width() and 1024 / 2 - level2img.get_height() / 2 <= event.pos[1] <= 1024 / 2 + level2img.get_height() / 2:
+                elif 128 + level1img.get_width() <= event.pos[0] <= 128 + 2 * level1img.get_width() and 1024 / 2 - level2img.get_height() / 2 <= event.pos[1] <= 1024 / 2 + level2img.get_height() / 2:
                     button_click_sound.play()
                     level2.main()
                     pygame.quit()
                     exit()
 
-                elif 1024 / 3 + level3img.get_width() + 128 <= event.pos[0] <= 1024 / 3 + 2 * level3img.get_width() + 128 and 1024 / 2 - level2img.get_height() / 2 <= event.pos[1] <= 1024 / 2 + level2img.get_height() / 2:
+                elif 192 + 2 * level2img.get_width() <= event.pos[0] <= 192 + 3 * level2img.get_width() and 1024 / 2 - level2img.get_height() / 2 <= event.pos[1] <= 1024 / 2 + level2img.get_height() / 2:
                     button_click_sound.play()
                     level3.main()
-                    pygame.quit()
                     exit()
+                    pygame.quit()
 
-        screen.blit(level1img, (1024 / 3 - level1img.get_width(), 1024 / 2 - level1img.get_height() / 2))
-        screen.blit(level2img, (1024 / 3 + 64, 1024 / 2 - level2img.get_height() / 2))
-        screen.blit(level3img, (1024 / 3 + level3img.get_width() + 128, 1024 / 2 - level1img.get_height() / 2))
+        screen.blit(level1img, (64, 1024 / 2 - level1img.get_height() / 2))
+        screen.blit(level2img, (128 + level1img.get_width(), 1024 / 2 - level2img.get_height() / 2))
+        screen.blit(level3img, (192 + 2 * level2img.get_width(), 1024 / 2 - level1img.get_height() / 2))
 
         clock.tick(60)
         pygame.display.update()

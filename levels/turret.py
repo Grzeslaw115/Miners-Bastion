cc1d6f2b57f7a6cbc98c3774a679f28e8a11bdb3
Krzysztof Gołuchowski <kgoluchowski@student.agh.edu.pl>
2024-04-07 14:58:39 +0200

    Add animation + money mechanics

--- levels/turret.py
+++ levels/turret.py
@@ -1,12 +1,16 @@
 import pygame as pg
 import constants as c
+import math
 
 
 class Turret(pg.sprite.Sprite):
     def __init__(self, sprite_sheet, tile_x, tile_y):
         pg.sprite.Sprite.__init__(self)
+        self.range = 150
         self.cooldown = 1500
         self.last_shot = pg.time.get_ticks()
+        self.selected = False
+        self.target = None
 
         self.tile_x = tile_x
         self.tile_y = tile_y
@@ -19,9 +23,20 @@ class Turret(pg.sprite.Sprite):
         self.frame_index = 0
         self.update_time = pg.time.get_ticks()
 
-        self.image = self.animation_list[self.frame_index]
+        self.angle = 90
+        self.org_image = self.animation_list[self.frame_index]
+        self.image = pg.transform.rotate(self.org_image, self.angle)
         self.rect = self.image.get_rect()
         self.rect.center = (self.x, self.y)
+
+        # Create range circle
+        self.range_image = pg.Surface((self.range * 2, self.range * 2))
+        self.range_image.fill((0, 0, 0))
+        self.range_image.set_colorkey((0, 0, 0))
+        pg.draw.circle(self.range_image, "gray100", (self.range, self.range), self.range)
+        self.range_image.set_alpha(100)
+        self.range_rect = self.range_image.get_rect()
+        self.range_rect.center = self.rect.center
 
     def load_images(self):
         size = self.sprite_sheet.get_height()
@@ -31,14 +46,38 @@ class Turret(pg.sprite.Sprite):
             animation_list.append(tmp_img)
         return animation_list
 
-    def update(self):
-        if pg.time.get_ticks() - self.last_shot > self.cooldown:
+    def update(self, enemy_group):
+        if self.target:
             self.play_animation()
+        else:
+            if pg.time.get_ticks() - self.last_shot > self.cooldown:
+                self.pick_target(enemy_group)
+
 
     def play_animation(self):
-        self.image = self.animation_list[self.frame_index]
+        self.org_image = self.animation_list[self.frame_index]
         if pg.time.get_ticks() - self.update_time > c.ANIMATION_DELAY:
             self.update_time = pg.time.get_ticks()
             self.frame_index = (self.frame_index + 1) % c.ANIMATION_STEPS
             if self.frame_index == 0:
                 self.last_shot = pg.time.get_ticks()
+                self.target = None
+
+    def draw(self, surface):
+        self.image = pg.transform.rotate(self.org_image, self.angle - 90)
+        self.rect = self.image.get_rect()
+        self.rect.center = (self.x, self.y)
+
+        surface.blit(self.image, self.rect)
+        if self.selected:
+            surface.blit(self.range_image, self.range_rect)
+
+    def pick_target(self, enemy_group):
+        for target in enemy_group:
+            x_dist = target.rect.centerx - self.x
+            y_dist = target.rect.centery - self.y
+            dist = math.sqrt(x_dist ** 2 + y_dist ** 2)
+            if dist < self.range:
+                self.target = target
+                self.angle = math.degrees(math.atan2(-y_dist, x_dist))
+                target.health -= 25
